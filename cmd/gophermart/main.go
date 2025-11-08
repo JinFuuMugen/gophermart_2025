8c2114223e85d2d2f4c8785b462b2da26072eb19
Alexey Kalinin <leshakalinin93@gmail.com>
2025-11-08 19:43:44 +0300

    initial commit

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/JinFuuMugen/gophermart-ya/config"
+	"github.com/JinFuuMugen/gophermart-ya/internal/api"
+	"github.com/JinFuuMugen/gophermart-ya/internal/logger"
+	"github.com/JinFuuMugen/gophermart-ya/internal/storage"
+)
+
+func main() {
+	if err := logger.InitLogger(); err != nil {
+		log.Fatalf("cannot init custom logger: %v", err)
+	}
+	defer logger.Sync()
+
+	cfg, err := config.LoadGophermartConfig()
+	if err != nil {
+		logger.Fatalf("cannot load config: %v", err)
+	}
+
+	db, err := storage.New(cfg.DatabaseURI)
+	if err != nil {
+		logger.Fatalf("cannot init database: %v", err)
+	}
+
+	jwtSecret := []byte("supersecretjwt")
+	router := api.InitRouter(db, jwtSecret)
+
+	logger.Infof("server starting at %s", cfg.RunAddress)
+
+	if err := http.ListenAndServe(cfg.RunAddress, router); err != nil {
+		logger.Fatalf("cannot start server: %v", err)
+	}
+}
