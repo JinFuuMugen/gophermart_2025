fe0480f8341003c9a5d2cbf6d83530f57b603bac
Alexey Kalinin <kalinin@buh-it-s.ru>
2025-12-04 11:13:00 +0300

    imports fix

--- internal/storage/psql.go
+++ internal/storage/psql.go
@@ -11,6 +11,8 @@ import (
 
 	"github.com/JinFuuMugen/gophermart-ya/internal/models"
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
